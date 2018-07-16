85b89e1f3cceb027ba6305176a1697c3039f38f8
Sebastian Sokolowski <sebo366@gmail.com>
2018-07-16 20:20:50 +0200

    Add new module to shared data between modules.

---
+++ MyApplication/shared/src/main/java/com/selastiansokolowski/shared/DataClientPaths.kt
@@ -0,0 +1,10 @@
+package com.selastiansokolowski.shared
+
+/**
+ * Created by Sebastian Sokołowski on 16.07.18.
+ */
+class DataClientPaths{
+    companion object {
+        val EXAMPLE_PATH = "/path"
+    }
+}
