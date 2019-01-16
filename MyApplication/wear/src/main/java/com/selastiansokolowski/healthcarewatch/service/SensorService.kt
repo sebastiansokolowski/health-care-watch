89b414952709be588108ebe8b0ba9f396ce95166
Sebastian Sokolowski <sebo366@gmail.com>
2019-01-16 23:54:25 +0100

    Register all needed sensors and send data by client.

--- MyApplication/wear/src/main/java/com/selastiansokolowski/healthcarewatch/service/SensorService.kt
+++ MyApplication/wear/src/main/java/com/selastiansokolowski/healthcarewatch/service/SensorService.kt
@@ -7,6 +7,8 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import com.selastiansokolowski.healthcarewatch.client.WearableDataClient
 import dagger.android.DaggerService
 import javax.inject.Inject
 
@@ -15,6 +17,10 @@ import javax.inject.Inject
  * Created by Sebastian Sokołowski on 09.07.18.
  */
 class SensorService : DaggerService(), SensorEventListener {
+    private val TAG = javaClass.canonicalName
+
+    @Inject
+    lateinit var wearableDataClient: WearableDataClient
 
     @Inject
     lateinit var sensorManager: SensorManager
@@ -25,6 +31,14 @@ class SensorService : DaggerService(), SensorEventListener {
 
     var hearthRateChangeListener: HearthRateChangeListener? = null
 
+    private val sensors = listOf(
+            Sensor.TYPE_HEART_RATE,
+            Sensor.TYPE_STEP_COUNTER,
+            Sensor.TYPE_PRESSURE,
+            Sensor.TYPE_GRAVITY,
+            Sensor.TYPE_LINEAR_ACCELERATION
+    )
+
     inner class LocalBinder : Binder() {
         internal val service: SensorService
             get() = this@SensorService
@@ -32,16 +46,6 @@ class SensorService : DaggerService(), SensorEventListener {
 
     override fun onBind(intent: Intent?): IBinder {
         return mBinder
-    }
-
-    lateinit var heartRateSensor: Sensor
-
-    override fun onCreate() {
-        super.onCreate()
-
-        sensorManager.apply {
-            heartRateSensor = getDefaultSensor(Sensor.TYPE_HEART_RATE)
-        }
     }
 
     fun toggleMeasurementState() {
@@ -58,30 +62,43 @@ class SensorService : DaggerService(), SensorEventListener {
     }
 
     private fun startMeasurement() {
-        sensorManager.apply {
-            registerListener(this@SensorService, heartRateSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        for (sensorId: Int in sensors) {
+            val sensor = sensorManager.getDefaultSensor(sensorId)
+            if (sensor == null) {
+                Log.d(TAG, "unavailable sensorId: $sensorId")
+                wearableDataClient.sendSensorSupportedInfo(sensorId, false)
+                continue
+            }
+            val registered = sensorManager.registerListener(this@SensorService, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            if (!registered) {
+                Log.d(TAG, "error register sensor: $sensorId")
+            }
+            wearableDataClient.sendSensorSupportedInfo(sensorId, registered)
         }
     }
 
     private fun stopMeasurement() {
-        sensorManager.apply {
-            unregisterListener(this@SensorService)
-        }
+        sensorManager.unregisterListener(this@SensorService)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        sensor?.apply {
+            wearableDataClient.sendSensorAccuracy(sensor, accuracy)
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
         event?.apply {
-            val sensorValue = values[0]
-
             when (event.sensor?.type) {
                 Sensor.TYPE_HEART_RATE -> {
+                    val sensorValue = values[0]
+
                     val heartRate = Math.round(sensorValue)
                     hearthRateChangeListener?.hearthRateChangeListener(heartRate.toString())
                 }
             }
+
+            wearableDataClient.sendSensorEvent(this)
         }
     }
 
