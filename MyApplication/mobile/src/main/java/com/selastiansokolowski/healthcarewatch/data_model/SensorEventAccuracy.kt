8b17ef81f4d5dd091ef94831b03343e2e1506e3a
Sebastian Sokolowski <sebo366@gmail.com>
2019-02-03 20:09:17 +0100

    Implement data models.

---
+++ MyApplication/mobile/src/main/java/com/selastiansokolowski/healthcarewatch/data_model/SensorEventAccuracy.kt
@@ -0,0 +1,6 @@
+package com.selastiansokolowski.healthcarewatch.data_model
+
+/**
+ * Created by Sebastian Sokołowski on 03.02.19.
+ */
+data class SensorEventAccuracy(val type: Int, val accuracy: Int)
