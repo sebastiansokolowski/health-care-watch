f24192c36f3da9766b7f70b2f73a8bf8ce591b61
Sebastian Sokolowski <sebo366@gmail.com>
2019-04-25 21:54:43 +0200

    Improve title on axis.

--- mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/DateValueFormatter.kt
+++ mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/DateValueFormatter.kt
@@ -2,24 +2,21 @@ package com.selastiansokolowski.healthcarewatch.view
 
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import java.sql.Time
 import java.text.SimpleDateFormat
 import java.util.*
 
 /**
  * Created by Sebastian Sokołowski on 10.03.19.
  */
-class DateValueFormatter() : IAxisValueFormatter {
+class DateValueFormatter : IAxisValueFormatter {
     var dtf1 = SimpleDateFormat("yyyyy-mm-dd")
-    var dtf2 = SimpleDateFormat("hh:mm:ss")
+    var dtf2 = SimpleDateFormat("HH:mm:ss")
 
     override fun getFormattedValue(value: Float, axis: AxisBase?): String {
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = value.toLong()
 
-        return if (calendar.get(Calendar.DAY_OF_MONTH) == Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) {
-            dtf2.format(calendar.time)
-        } else {
-            dtf1.format(calendar.time)
-        }
+        return dtf2.format(calendar.time)
     }
 }
