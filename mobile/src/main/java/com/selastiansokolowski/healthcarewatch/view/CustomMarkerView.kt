635ca09f306dd09830940e61e2c6678d2807186e
Sebastian Sokolowski <sebo366@gmail.com>
2019-06-14 20:21:32 +0200

    Show date in marker.

--- mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/CustomMarkerView.kt
+++ mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/CustomMarkerView.kt
@@ -1,10 +1,13 @@
 package com.selastiansokolowski.healthcarewatch.view
 
 import android.content.Context
+import android.view.View
 import com.github.mikephil.charting.components.MarkerView
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.utils.MPPointF
+import com.selastiansokolowski.healthcarewatch.util.EntryHelper
+import com.selastiansokolowski.healthcarewatch.util.SafeCall
 import kotlinx.android.synthetic.main.custom_marker_view.view.*
 
 /**
@@ -13,7 +16,10 @@ import kotlinx.android.synthetic.main.custom_marker_view.view.*
 class CustomMarkerView(context: Context, layoutId: Int) : MarkerView(context, layoutId) {
     override fun refreshContent(e: Entry?, highlight: Highlight?) {
 
-        text.text = e?.y.toString()
+        SafeCall.safeLet(e?.x, e?.y) { x, y ->
+            text.text = "${EntryHelper.getDate(x)}\n$y"
+            text.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        }
 
         super.refreshContent(e, highlight)
     }
--- mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/DateValueFormatter.kt
+++ mobile/src/main/java/com/selastiansokolowski/healthcarewatch/view/DateValueFormatter.kt
@@ -2,26 +2,14 @@ package com.selastiansokolowski.healthcarewatch.view
 
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
-import java.text.SimpleDateFormat
-import java.util.*
+import com.selastiansokolowski.healthcarewatch.util.EntryHelper
 
 /**
  * Created by Sebastian Sokołowski on 10.03.19.
  */
 class DateValueFormatter : IAxisValueFormatter {
-    var dtf1 = SimpleDateFormat("yyyyy-mm-dd")
-    var dtf2 = SimpleDateFormat("HH:mm:ss")
-
 
     override fun getFormattedValue(value: Float, axis: AxisBase?): String {
-        val timestampFromMidnight = value.toInt()
-
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, 0)
-        calendar.set(Calendar.MINUTE, 0)
-        calendar.set(Calendar.SECOND, 0)
-        calendar.timeInMillis += timestampFromMidnight
-
-        return dtf2.format(calendar.time)
+        return EntryHelper.getDate(value)
     }
 }
