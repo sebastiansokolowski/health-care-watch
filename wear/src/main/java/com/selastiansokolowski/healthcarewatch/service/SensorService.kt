ab557810b3c5387614ac0a0e21a5eb2c62d2feb0
Sebastian Sokolowski <sebo366@gmail.com>
2019-06-23 02:43:42 +0200

    Stop measurement when SensorService will be destroyed.

--- wear/src/main/java/com/selastiansokolowski/healthcarewatch/service/SensorService.kt
+++ wear/src/main/java/com/selastiansokolowski/healthcarewatch/service/SensorService.kt
@@ -3,6 +3,7 @@ package com.selastiansokolowski.healthcarewatch.service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
 import com.selastiansokolowski.healthcarewatch.model.SensorDataModel
 import dagger.android.DaggerService
 import javax.inject.Inject
@@ -12,6 +13,7 @@ import javax.inject.Inject
  * Created by Sebastian Sokołowski on 09.07.18.
  */
 class SensorService : DaggerService() {
+    private val TAG = javaClass.canonicalName
 
     @Inject
     lateinit var sensorDataModel: SensorDataModel
@@ -27,4 +29,14 @@ class SensorService : DaggerService() {
         return mBinder
     }
 
+    override fun onCreate() {
+        Log.d(TAG, "onCreate")
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        sensorDataModel.stopMeasurement()
+        super.onDestroy()
+    }
 }
