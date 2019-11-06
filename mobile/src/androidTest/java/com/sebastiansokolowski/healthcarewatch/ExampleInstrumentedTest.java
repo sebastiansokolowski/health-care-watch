b69c363b8082cd6cec96b211d001e93950da634d
Sebastian Sokolowski <sebo366@gmail.com>
2019-11-06 20:55:38 +0100

    Fix typo in package name.

--- mobile/src/androidTest/java/com/sebastiansokolowski/healthcarewatch/ExampleInstrumentedTest.java
+++ mobile/src/androidTest/java/com/sebastiansokolowski/healthcarewatch/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.selastiansokolowski.healthcarewatch", appContext.getPackageName());
+        assertEquals("com.sebastiansokolowski.healthcarewatch", appContext.getPackageName());
     }
 }
