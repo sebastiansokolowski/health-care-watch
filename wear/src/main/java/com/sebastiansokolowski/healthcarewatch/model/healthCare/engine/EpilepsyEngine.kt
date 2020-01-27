b1cbfb47ef756cf9ddeefc80f30a3a11fd519f05
Sebastian Sokolowski <sebo366@gmail.com>
2020-01-27 22:22:21 +0100

    Implement init version of epilepsy engine.

--- wear/src/main/java/com/sebastiansokolowski/healthcarewatch/model/healthCare/engine/EpilepsyEngine.kt
+++ wear/src/main/java/com/sebastiansokolowski/healthcarewatch/model/healthCare/engine/EpilepsyEngine.kt
@@ -1,26 +1,71 @@
 package com.sebastiansokolowski.healthcarewatch.model.healthCare.engine
 
 import android.hardware.Sensor
+import android.util.Log
 import com.sebastiansokolowski.healthcarewatch.model.healthCare.HealthCareEngineBase
 import com.sebastiansokolowski.shared.dataModel.HealthCareEventType
+import com.sebastiansokolowski.shared.dataModel.SensorEvent
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Created by Sebastian Sokołowski on 07.06.19.
  */
 class EpilepsyEngine : HealthCareEngineBase() {
+    val TAG = this::class.java.simpleName
 
-    override fun requiredSensors(): Set<Int> {
-        return setOf(Sensor.TYPE_HEART_RATE)
-    }
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    private data class AcceDataModel(val sensorEvent: SensorEvent, val acceCurrent: Double)
 
     override fun startEngine() {
+        sensorEventSubject
+                .subscribeOn(Schedulers.io())
+                .filter { it.type == Sensor.TYPE_LINEAR_ACCELERATION }
+                .buffer(TimeUnit.SECONDS.toMillis(measurementSettings.epilepsySettings.timeS.toLong()),
+                        measurementSettings.samplingMs.toLong(),
+                        TimeUnit.MILLISECONDS)
+                .map {
+                    val acceDataList = mutableListOf<AcceDataModel>()
+                    it.forEach {
+                        val acce =
+                                sqrt(
+                                        it.values[0].toDouble().pow(2.0) +
+                                                it.values[1].toDouble().pow(2.0) +
+                                                it.values[2].toDouble().pow(2.0)
+                                )
+                        acceDataList.add(AcceDataModel(it, acce))
+                    }
+
+                    acceDataList
+                }
+                .subscribe {
+                    it.forEach {
+                        if (it.acceCurrent < measurementSettings.epilepsySettings.threshold) {
+                            return@subscribe
+                        }
+                    }
+
+                    Log.d(TAG, "epilepsy detected!!")
+                    notifyHealthCareEvent(it.last().sensorEvent)
+                }
+                .let {
+                    compositeDisposable.add(it)
+                }
     }
 
     override fun stopEngine() {
+        compositeDisposable.clear()
     }
 
     override fun getHealthCareEventType(): HealthCareEventType {
         return HealthCareEventType.EPILEPSY
     }
 
+    override fun requiredSensors(): Set<Int> {
+        return setOf(Sensor.TYPE_LINEAR_ACCELERATION)
+    }
 }
