69c5d670d381a1a78770f5e3082c41758b57d5d5
Sebastian Sokolowski <sebo366@gmail.com>
2020-03-22 17:51:27 +0100

    Add auto restart when settings has been changed.

--- mobile/src/main/java/com/sebastiansokolowski/healthguard/viewModel/AdvancedSettingsViewModel.kt
+++ mobile/src/main/java/com/sebastiansokolowski/healthguard/viewModel/AdvancedSettingsViewModel.kt
@@ -1,32 +1,19 @@
 package com.sebastiansokolowski.healthguard.viewModel
 
 import android.arch.lifecycle.ViewModel
-import android.content.ContentResolver
-import android.content.Context
 import com.sebastiansokolowski.healthguard.model.SensorDataModel
-import com.sebastiansokolowski.healthguard.model.SettingsModel
-import com.sebastiansokolowski.healthguard.model.SetupModel
-import com.sebastiansokolowski.healthguard.util.HealthEventHelper
-import com.sebastiansokolowski.shared.SettingsSharedPreferences
 import javax.inject.Inject
 
 /**
  * Created by Sebastian Sokołowski on 10.03.19.
  */
 class AdvancedSettingsViewModel
-@Inject constructor(context: Context, private val settingsModel: SettingsModel, private val sensorDataModel: SensorDataModel, private val contentResolver: ContentResolver, val setupModel: SetupModel) : ViewModel() {
-
-    private val healthEventHelper = HealthEventHelper(context)
+@Inject constructor(private val sensorDataModel: SensorDataModel) : ViewModel() {
 
     fun onSharedPreferenceChanged(key: String) {
-        when (key) {
-            SettingsSharedPreferences.SAMPLING_US,
-            SettingsSharedPreferences.HEALTH_EVENTS -> {
-                if (sensorDataModel.measurementRunning) {
-                    sensorDataModel.stopMeasurement()
-                    sensorDataModel.requestStartMeasurement()
-                }
-            }
+        if (sensorDataModel.measurementRunning) {
+            sensorDataModel.stopMeasurement()
+            sensorDataModel.requestStartMeasurement()
         }
     }
 
