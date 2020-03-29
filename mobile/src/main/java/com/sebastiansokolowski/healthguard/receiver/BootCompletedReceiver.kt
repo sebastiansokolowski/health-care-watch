b2109e22244c95f10b97314beffdbf22992135e4
Sebastian Sokolowski <sebo366@gmail.com>
2020-03-29 12:53:13 +0200

    Start services after the boot is completed in the mobile app.

---
+++ mobile/src/main/java/com/sebastiansokolowski/healthguard/receiver/BootCompletedReceiver.kt
@@ -0,0 +1,24 @@
+package com.sebastiansokolowski.healthguard.receiver
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.sebastiansokolowski.healthguard.service.MessageReceiverService
+import dagger.android.DaggerBroadcastReceiver
+
+
+/**
+ * Created by Sebastian Sokołowski on 04.06.19.
+ */
+class BootCompletedReceiver : DaggerBroadcastReceiver() {
+    private val TAG = javaClass.canonicalName
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d(TAG, "onReceive action=${intent?.action}")
+        super.onReceive(context, intent)
+
+        context?.apply {
+            startService(Intent(this, MessageReceiverService::class.java))
+        }
+    }
+}
