065a2e5ec027ff84eb85496a1ed9048c5e934d70
Sebastian Sokołowski <sebo366@gmail.com>
2020-06-10 01:56:25 +0200

    Fix formatting small float values as -0.

--- shared/src/main/java/com/sebastiansokolowski/shared/util/Utils.kt
+++ shared/src/main/java/com/sebastiansokolowski/shared/util/Utils.kt
@@ -7,10 +7,17 @@ import java.text.DecimalFormat
  */
 class Utils {
     companion object {
-        fun format(float: Float, digits: Int): String {
+        fun format(float: Float, maximumFractionDigits: Int): String {
             val df = DecimalFormat()
-            df.maximumFractionDigits = digits
-            return df.format(float)
+            df.minimumFractionDigits = 0
+            df.maximumFractionDigits = maximumFractionDigits
+            val result = df.format(float)
+            return if (result.replace(",", ".").toFloatOrNull() == 0.0f) {
+                df.negativePrefix = ""
+                df.format(float)
+            } else {
+                result
+            }
         }
 
     }
