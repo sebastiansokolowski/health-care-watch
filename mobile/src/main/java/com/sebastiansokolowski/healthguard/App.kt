90bc38ce2f150d4d8dee57bb0b36aa7600c14a7e
Sebastian Sokołowski <sebo366@gmail.com>
2020-06-10 03:08:03 +0200

    Implement save logs to the external storage.

--- mobile/src/main/java/com/sebastiansokolowski/healthguard/App.kt
+++ mobile/src/main/java/com/sebastiansokolowski/healthguard/App.kt
@@ -1,6 +1,7 @@
 package com.sebastiansokolowski.healthguard
 
 import com.sebastiansokolowski.healthguard.di.DaggerAppComponent
+import com.sebastiansokolowski.healthguard.util.LogUtils
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
@@ -8,6 +9,12 @@ import dagger.android.DaggerApplication
  * Created by Sebastian Sokołowski on 08.07.18.
  */
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        LogUtils.setupLogcat(applicationInfo)
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder()
---
+++ mobile/src/main/java/com/sebastiansokolowski/healthguard/util/LogUtils.kt
@@ -0,0 +1,31 @@
+package com.sebastiansokolowski.healthguard.util
+
+import android.content.pm.ApplicationInfo
+import android.os.Environment
+import com.sebastiansokolowski.healthguard.BuildConfig
+import java.io.File
+import java.io.IOException
+import java.util.*
+
+/**
+ * Created by Sebastian Sokołowski on 29.10.19.
+ */
+class LogUtils {
+    companion object {
+        fun setupLogcat(applicationInfo: ApplicationInfo) {
+            if (0 != (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE)) {
+                val file = File(Environment.getExternalStorageDirectory(), "health_guard")
+                if (!file.exists()) {
+                    file.mkdirs()
+                }
+
+                val filePath: String = Environment.getExternalStorageDirectory().path +
+                        String.format("/health_guard/logcat_%s.txt", Calendar.getInstance().time)
+                try {
+                    Runtime.getRuntime().exec(arrayOf("logcat", "*:D", "-v", "time", "-f", filePath))
+                } catch (e: IOException) {
+                }
+            }
+        }
+    }
+}
