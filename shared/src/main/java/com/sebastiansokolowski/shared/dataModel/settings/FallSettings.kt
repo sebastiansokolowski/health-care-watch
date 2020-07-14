731152456a42c0d59f03b351e662494dbe26fc8f
Sebastian Sokołowski <sebo366@gmail.com>
2020-07-15 00:57:48 +0200

    Update the fall settings.

--- shared/src/main/java/com/sebastiansokolowski/shared/dataModel/settings/FallSettings.kt
+++ shared/src/main/java/com/sebastiansokolowski/shared/dataModel/settings/FallSettings.kt
@@ -3,5 +3,5 @@ package com.sebastiansokolowski.shared.dataModel.settings
 /**
  * Created by Sebastian Sokołowski on 20.01.20.
  */
-data class FallSettings(val threshold: Int = 23, val samplingTimeS: Int = 6,
-                        val inactivityDetector: Boolean = true, val inactivityDetectorTimeoutS: Int = 1, val inactivityDetectorThreshold: Int = 3)
+data class FallSettings(val threshold: Int = 22, val samplingTimeS: Int = 7,
+                        val inactivityDetector: Boolean = true, val inactivityDetectorTimeoutS: Int = 1, val inactivityDetectorThreshold: Int = 4)
