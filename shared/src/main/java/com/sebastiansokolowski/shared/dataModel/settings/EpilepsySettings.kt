c066cd064a08669281d1da29de1c4ff55e370c37
Sebastian Sokołowski <sebo366@gmail.com>
2020-07-15 00:57:57 +0200

    Update the epilepsy settings.

--- shared/src/main/java/com/sebastiansokolowski/shared/dataModel/settings/EpilepsySettings.kt
+++ shared/src/main/java/com/sebastiansokolowski/shared/dataModel/settings/EpilepsySettings.kt
@@ -3,4 +3,4 @@ package com.sebastiansokolowski.shared.dataModel.settings
 /**
  * Created by Sebastian Sokołowski on 22.01.20.
  */
-data class EpilepsySettings(val threshold: Int = 6, val samplingTimeS: Int = 7, val motions: Int = 100)
+data class EpilepsySettings(val threshold: Int = 6, val samplingTimeS: Int = 7, val motions: Int = 150)
